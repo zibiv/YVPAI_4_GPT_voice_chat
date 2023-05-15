2d94308cafe612593932cd52e6088b4e1fa56947
GenIl <inmydreamsin@gmail.com>
2023-05-15 17:50:42 +0300

    feat: создать класс для запросов к OpenAI feat: получить ответ из чата OpenAI feat: сохранить контекст разговора
    OpenAI класс отвечает за трансляцию аудио в текст
    + отправку запроса к чату и получения ответа
    Контекст поддерживается пока для одного пользователя

--- src/AI/openai.js
+++ src/AI/openai.js
@@ -2,14 +2,41 @@ import { Configuration, OpenAIApi } from 'openai'
 import config from 'config'
 import fs from 'fs'
 
-const configuration = new Configuration({
-  apiKey: config.get('OPENAI_TOKEN'),
-})
+class OpenAI {
+  roles = {
+    ASSISTANT: 'assistant',
+    USER: 'user',
+    SYSTEM: "system",
+  }
+  constructor(api_key) {
+    const configuration = new Configuration({
+      apiKey: api_key,
+    })
+    this.API = new OpenAIApi(configuration)
+  }
 
-const openAI = new OpenAIApi(configuration)
+  async chatAI(messages) {
+    const responseFromOpenAI = await this.API.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+      temperature: 0.3,
+      frequency_penalty: 0.1,
+      presence_penalty: -1,
+      max_tokens: 500,
+      stream: false,
+      n: 1,
+    })
+    return responseFromOpenAI.data.choices[0].message
+  }
 
-export async function translateAI(filePath) {
-  const audioFile = fs.createReadStream(filePath)
-  const transcript = await openAI.createTranscription(audioFile, 'whisper-1')
-  return transcript.data.text
+  async transcriptAI(filePath) {
+    const audioFile = fs.createReadStream(filePath)
+    const transcript = await this.API.createTranscription(
+      audioFile,
+      'whisper-1'
+    )
+    return transcript.data.text
+  }
 }
+
+export const openAIApi = new OpenAI(config.get('OPENAI_TOKEN'))
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import config from 'config'
 import { ogg } from './voice/ogg.js'
-
-import { translateAI } from './AI/openai.js'
+import { openAIApi } from './AI/openai.js'
 
 import { Telegraf } from 'telegraf' //это экземпляр моего бота
 import { message } from 'telegraf/filters'
+
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
+const messages = [{role: openAIApi.roles.USER, content: 'Ты очень умный кот помощник, который умеет разговаривать. Ты готов ответить на любой вопрос человека. Тебя зовут Ферик, больше всего на свете ты любишь сидеть на подоконнике и смотреть на улицу! На вопросы давай емкие ответы по существу. Будь любезен!'}]
 
 // bot.on("message", (ctx) => ctx.reply(JSON.stringify(ctx.message, null, 2)))
 bot.start((ctx) => {
@@ -22,11 +23,18 @@ bot.on(message('voice'), async (ctx) => {
   const fileLinkObj  = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
 
   try {
+    await ctx.sendChatAction("typing")
     //конвертируем из ogg в mp3
     const map3Path = await ogg.convert(fileLinkObj.href, userId)
     //получаем транскрипцию из аудио
-    const audioText = await translateAI(map3Path)
-    await ctx.sendMessage(audioText)
+    //TODO не сохранять файл, а направлять сразу поток
+    const audioText = await openAIApi.transcriptAI(map3Path)
+    messages.push({role: openAIApi.roles.USER, content: audioText})
+    const chatAnswer = await openAIApi.chatAI(messages)
+    //TODO давать ответ в том числе голосом 
+    messages.push(chatAnswer)
+    console.log(chatAnswer)
+    await ctx.sendMessage(chatAnswer.content)
   } catch(error) {
     console.error(error);
     await ctx.sendMessage('Ваш запрос полностью провалился, уже исправляем это.')
@@ -41,3 +49,4 @@ const handleStopSignal = () => {
 
 process.once('SIGINT', handleStopSignal)
 process.once('SIGTERM', handleStopSignal)
+
