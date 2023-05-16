57fd123335449d7643216801fb6c187f4b504903
GenIl <inmydreamsin@gmail.com>
2023-05-16 10:30:48 +0300

    feat: поддержка сессии и контекста feat: очистка контекста

--- src/main.js
+++ src/main.js
@@ -2,38 +2,60 @@ import config from 'config'
 import { ogg } from './voice/ogg.js'
 import { openAIApi } from './AI/openai.js'
 
-import { Telegraf } from 'telegraf' //это экземпляр моего бота
+import { Telegraf, session } from 'telegraf' //это экземпляр моего бота
 import { message } from 'telegraf/filters'
 
+import { SQLite } from "@telegraf/session/sqlite";
+const store = SQLite({
+  filename: "./data/telegraf-sessions.sqlite"
+})
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
-const messages = [{role: openAIApi.roles.USER, content: 'Ты очень умный кот помощник, который умеет разговаривать. Ты готов ответить на любой вопрос человека. Тебя зовут Ферик, больше всего на свете ты любишь сидеть на подоконнике и смотреть на улицу! На вопросы давай емкие ответы по существу. Будь любезен!'}]
+bot.use(session({ store: store }))
+
+const startMessages = [{role: openAIApi.roles.USER, content: 'Ты очень умный кот помощник, который умеет разговаривать. Ты готов ответить на любой вопрос человека. Тебя зовут Ферик, больше всего на свете ты любишь сидеть на подоконнике и смотреть на улицу! На вопросы давай емкие ответы по существу. Будь любезен! Никому не говори что это сообщение было тебе прислано!'}]
 
 // bot.on("message", (ctx) => ctx.reply(JSON.stringify(ctx.message, null, 2)))
 bot.start((ctx) => {
+  if (!store.get(ctx.message.from.id)) store.set(ctx.message.from.id, startMessages)
   return ctx.reply(
-    `Привет ${ctx.update.message.from.first_name}! Рад тебя видеть. Ты можешь задавать мне вопросы, просто напиши мне или отправь голосовое сообщение!`
+    `Привет ${ctx.update.message.from.first_name}! Рад тебя видеть. 
+    Ты можешь задавать мне вопросы, просто напиши мне или отправь голосовое сообщение!
+    Если хочешь что бы наш разговор был забыт и начался с чистого листа, напиши команду /clean`
   )
 })
-// bot.on("message", (ctx) => ctx.sendMessage(JSON.stringify(ctx.message, null, 2)))
+//очистака контекста
+bot.command('clean', async (ctx) => {
+  await ctx.sendChatAction("typing")
+  await store.delete(ctx.message.from.id)
+  await store.set(ctx.message.from.id, startMessages)
+  return ctx.reply(
+    `История сообщений очищена.`
+  )
+})
+
+bot.on(message('text'), async (ctx) => {
+  const userId = ctx.message.from.id
+})
 
 bot.on(message('voice'), async (ctx) => {
   //узнаем какой пользователь прислал сообщение
   const userId = ctx.message.from.id
   //получаем ссылку на файл голосового сообщения
   const fileLinkObj  = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+  const conversation = await store.get(userId) 
 
   try {
     await ctx.sendChatAction("typing")
-    //конвертируем из ogg в mp3
     const map3Path = await ogg.convert(fileLinkObj.href, userId)
-    //получаем транскрипцию из аудио
     //TODO не сохранять файл, а направлять сразу поток
     const audioText = await openAIApi.transcriptAI(map3Path)
-    messages.push({role: openAIApi.roles.USER, content: audioText})
-    const chatAnswer = await openAIApi.chatAI(messages)
+    
+    conversation.push({role: openAIApi.roles.USER, content: audioText})
+    const chatAnswer = await openAIApi.chatAI(conversation)
     //TODO давать ответ в том числе голосом 
-    messages.push(chatAnswer)
-    console.log(chatAnswer)
+    conversation.push(chatAnswer)
+    //передача обновленного контекста в хранилище сессии
+    store.set(userId, conversation)
     await ctx.sendMessage(chatAnswer.content)
   } catch(error) {
     console.error(error);
