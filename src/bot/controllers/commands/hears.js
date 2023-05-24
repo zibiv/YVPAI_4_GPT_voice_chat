8c3173b6d73d8a20365040245f10d29ddb89cea7
GenIl <inmydreamsin@gmail.com>
2023-05-24 11:11:10 +0300

    refactor: смена режима не теряет общий контекст
    Такими образом переключившись обратно в chatters режим, можно обсудить
    улучшенный текст
    или же разбор ответа на вопрос

--- src/bot/controllers/commands/hears.js
+++ src/bot/controllers/commands/hears.js
@@ -3,7 +3,6 @@ import { createNewContext } from "../../../../data/starterMessage.js"
 
 const betterText = async (ctx) => {
   userMode.setMode('betterText')
-  ctx.session = createNewContext()
   ctx.reply('Включаю режим улучшения текста, жду вашего сообщения содержащего текст для улучшения, исправления орфографии.', {
     reply_markup: { remove_keyboard: true },
   })
@@ -24,7 +23,6 @@ const assistant = async (ctx) => {
 
 const chatter = async (ctx) => {
   userMode.setMode('chatter')
-  ctx.session = createNewContext()
   ctx.reply('Включаю режим чаттер, в котором мы общаемся на различные темы, жду ваше аудио сообщение или текст.', {
     reply_markup: { remove_keyboard: true },
   })
@@ -32,7 +30,6 @@ const chatter = async (ctx) => {
 
 const checkAnswer = async (ctx) => {
   userMode.setMode('checkAnswer')
-  ctx.session = createNewContext()
   ctx.reply('Включаю режим проверки ответов. Этот режим поможет проверить, насколько хорошо вы понимаете ту или иную тему.', {
     reply_markup: { remove_keyboard: true },
   })
